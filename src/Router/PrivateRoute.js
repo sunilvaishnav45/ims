d36ce4341f4adf834a31ec8dc872d6c15ee54b61
Sunil <sunil@Sunils-MacBook-Air.local>
2020-05-10 22:24:26 +0530

    Auth token

--- src/Router/PrivateRoute.js
+++ src/Router/PrivateRoute.js
@@ -3,15 +3,13 @@ import { Route, Redirect } from 'react-router-dom';
 import LoginService from '../Services/LoginService';
 
 var loginService = new LoginService()
-var REACT_APP_ROOT_CONTEXT = process.env.REACT_APP_ROOT_CONTEXT;
-var loginPageUrl = REACT_APP_ROOT_CONTEXT+"/#/login"
 
 const PrivateRoute = ({component: Component, ...rest}) => {
     return (
         <Route {...rest} render={props => (
             loginService.isUserLoggedIn() ?
                 <Component {...props} />
-            : <Redirect to={loginPageUrl} />
+            : <Redirect to="/login" />
         )} />
     );
 };
