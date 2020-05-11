3cbadaaf47ac63734898dffc6c6290ab53ae7e2a
Sunil <sunil@Sunils-MacBook-Air.local>
2020-05-11 12:13:13 +0530

    Auth token

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ims-web/precache-manifest.9d430aebf062caac6dd2447934c34384.js"
+  "/ims-web/precache-manifest.2756d194fd8241ed76767964be1e0d23.js"
 );
 
 self.addEventListener('message', (event) => {
